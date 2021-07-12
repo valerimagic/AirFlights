2f6d05b6637472a1caf7776ccade64187db06a9d
Valeri Hristov <valerimagic@gmail.com>
2021-07-12 12:54:52 +0300

    p

---
+++ AirFlights/DAL/Connections/AirFlightConnString.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Connections
+{
+    class AirFlightConnString
+    {
+        internal const string DefaultConnection =
+            "Server=DESKTOP-Q9B2B1D\\SQLEXPRESS;Database=Airflights;Integrated Security=True;";
+    }
+}
---
+++ AirFlights/DAL/Contexts/AirFlightContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Connections;
+using DAL.Entities;
+
+namespace DAL.Contexts
+{
+    public class AirFlightContext : DbContext
+    {
+        public DbSet<Airplanes> Airplaneses { get; set; }
+        public DbSet<Flights> Flightses { get; set; }
+        public DbSet<Passengers> Passengerses { get; set; }
+        public DbSet<RegistrationDetails> RegistrationDetailses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(AirFlightConnString.DefaultConnection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
